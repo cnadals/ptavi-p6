9bb8b8b5304929fdab10369302cd75be93452765
cnadals <carmen.nadal@gmail.com>
2016-11-14 10:58:55 +0100

    principio

--- client.py
+++ client.py
@@ -9,19 +9,19 @@ import socket
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+server = 'localhost'
+port = 6001
 
 # Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+line = '¡Hola mundo!'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+my_socket.connect((server, port))
 
-print("Enviando: " + LINE)
-my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+print("Enviando: " + line)
+my_socket.send(bytes(line, 'utf-8') + b'\r\n')
 data = my_socket.recv(1024)
 
 print('Recibido -- ', data.decode('utf-8'))
