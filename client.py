057fa22bbe254d09479b0a2733270ee6d6a965a0
cnadals <carmen.nadal@gmail.com>
2016-11-18 13:34:45 +0100

    client para revisar respuesta mp3

--- client.py
+++ client.py
@@ -19,26 +19,26 @@ direccion = direccion.split(':')[0]
 
 #EXCEPCION ESTAN BIEN/NO LOS ARGUMENTOS
 if not len(sys.argv) == 3:
-    sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
+    sys.exit('Usage: python3 client.py method receiver@IP:SIPport')
 _, metodo, direccion = sys.argv
 
 if metodo == 'INVITE':
 	line = metodo + ' sip:' + direccion + ' SIP/2.0'
-	print("Enviando: " + line)
+	print('Enviando: ' + line)
 if metodo == 'BYE':
 	line = metodo + ' sip:' + direccion + ' SIP/2.0'
-	print("Enviando: " + line)
+	print('Enviando: ' + line)
 if metodo == 'ACK':
 	line = metodo + ' sip:' + direccion + ' SIP/2.0'
-	print("Enviando: " + line)
+	print('Enviando: ' + line)
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.connect(('127.0.0.1', port))
 my_socket.send(bytes(line, 'utf-8') + b'\r\n')
 data = my_socket.recv(1024)
 
 print('Recibido -- ', data.decode('utf-8'))
-print("Terminando socket...")
+print('Terminando socket...')
 
 # Cerramos todo
 my_socket.close()
-print("Fin.")
+print('Fin.')
